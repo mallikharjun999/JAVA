454f2b895df28a5aac8f4c938fc2c67fb2c575eb
mallikharjun999 <mallikharjunandey999@gmail.com>
2024-07-02 21:57:16 +0530

    first commit

---
+++ Day1.java
@@ -0,0 +1,7 @@
+public class Day1 {
+    public static void main(String[] args) {
+        System.out.println("Day one of java");
+        System.out.println("All the best for your journey");
+    }
+}
+    
---
+++ Day2.java
@@ -0,0 +1,10 @@
+public class Day2 {
+    public static void main(String[] args) {
+        int a=10;
+        char c = 'v';
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println("variable a is" +a);
+    }
+    
+}
